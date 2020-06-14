ecba7de8c14720f585f4ec333570be45f5f469c5
Glitch (youthful-same-bed) <none>
2020-06-14 16:06:28 +0000

    🏈🙌 Checkpoint ./package.json:16213770/1572 ./commands/economy/bal.js:16213770/320

---
+++ commands/economy/bal.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+
+exports.run = async (client, message, args) => {
+  
+}
+
+exports.help = {
+  name: "",
+  description: "",
+  usage: "",
+  example: ""
+};
+
+exports.conf = {
+  aliases: "bal",
+  cooldown: 5
+}
