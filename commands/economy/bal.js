acc951ab78b565929b14bd5bc003326e695e2fc4
Glitch (youthful-same-bed) <none>
2020-06-14 16:20:26 +0000

    🎎🏑 Checkpoint ./commands/economy/module.json:16213770/68 ./commands/economy/bal.js:16213770/835 ./commands/economy/daily.js:16213770/988

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -2,17 +2,22 @@ const Discord = require("discord.js")
 const db = require("quick.db")
 
 exports.run = async (client, message, args) => {
-  
+  let user = message.mentions.users.first() || message.author
+        let money = db.fetch(`money_${user.id}`)
+
+        if (money === null) money = 0
+
+        message.channel.send(`${user} you have ${money} coins`)
 }
 
 exports.help = {
-  name: "",
-  description: "",
-  usage: "",
-  example: ""
+  name: "bal",
+  description: "balance ur money",
+  usage: "/bal <mention>",
+  example: "/bal @mention someone"
 };
 
 exports.conf = {
-  aliases: "bal",
+  aliases: ["bal"],
   cooldown: 5
-}
+};
---
+++ commands/economy/daily.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+const ms = require("parse-ms")
+
+exports.run = async (client, message, args) => {
+  let daily = await db.fetch(`daily_${message.author.id}`);
+
+        if (daily != null && timeout - (Date.now() - daily) > 0) {
+            let time = ms(timeout - (Date.now() - daily));
+            message.channel.send(`You already collected your daily reward, you can come back in **${time.hours}h ${time.minutes}m ${time.seconds}s**`)
+
+
+        } else {
+            let embed = new Discord.MessageEmbed()
+            .setAuthor(`Daily`, message.author.displayAvatarURL)
+            .setColor("GREEN")
+            .setDescription(`**Daily Rewards**`)
+            .addField(`Collected`, amount)
+            message.channel.send(embed)
+
+            db.add(`money_${message.author.id}`, amount)
+            db.add(`daily_${message.author.id}`, Date.now())
+        }
+}
