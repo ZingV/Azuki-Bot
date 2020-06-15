3da4b2d104b3e00127bb39732ea3b36e11ed3173
Glitch (youthful-same-bed) <none>
2020-06-15 03:28:36 +0000

    🔨🏦 Checkpoint ./commands/utility/covid.js:16213770/81 ./commands/admin/addrole.js:16213770/337

--- commands/utility/covid.js
+++ commands/utility/covid.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
 const fetch = require("node-fetch");
 const config = require("../../config.json");
 
