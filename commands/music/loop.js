1f5c416da741b645025e212376fbc45c385f7a2c
Glitch (youthful-same-bed) <none>
2020-06-15 06:14:22 +0000

    🙉💃 Checkpoint ./commands/music/play.js:16213770/349 ./commands/music/module.json:16213770/54 ./config.json:16213770/3 ./commands/music/loop.js:16213770/284

---
+++ commands/music/loop.js
@@ -0,0 +1,14 @@
+exports.run = async (client, message, args) => {
+}
+
+exports.help = {
+         name: "loop",
+         description: "looping ur music queue",
+         usage: "/loop",
+         example: "/loop",
+};
+
+exports.conf = {
+          aliases: [""],
+          cooldown: 5
+};
---
+++ commands/music/play.js
@@ -0,0 +1,15 @@
+exports.run = async (client, message, args) => {
+  
+}
+
+exports.help = {
+         name: "play",
+         description: "to play music",
+         usage: "/p <url/music name>",
+         example: "/p lily",
+};
+
+exports.conf = {
+          aliases: ["p"],
+          cooldown: 5
+};
