133ea0a7cf8009b1fce1a74bb2f8b70f5028a601
Glitch (youthful-same-bed) <none>
2020-06-15 05:45:35 +0000

    🍍👲 Checkpoint ./config.json:16213770/3 ./commands/utility/invite.js:16213770/417

---
+++ commands/utility/invite.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args) => {
+  
+}
+
+exports.help = {
+  name: "invite",
+  description: "invite the bot to ur server!",
+  usage: "/invite",
+  example: "/invite"
+}
+
+exports.conf = {
+  aliases: [""],
+  cooldown: 5
+}
