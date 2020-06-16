3a2c27991897f8fe3a5d2d1ee812c973e8b512d0
Glitch (youthful-same-bed) <none>
2020-06-16 14:15:09 +0000

    🍪🎮 Checkpoint ./commands/fun/hack.js:16213770/5262

--- commands/fun/hack.js
+++ commands/fun/hack.js
@@ -1,89 +1,96 @@
-const Discrod = require('discord.js')
-const ms = require('ms')
+const Discrod = require("discord.js");
+const ms = require("ms");
 
 exports.run = async (client, message, args, ops) => {
-if (!args[0]) return message.channel.send("<:redtick:719865119277842492> | Please mention the users!");
-  if(message.author.id === ops.ownerID) return message.channel.send("<a:b_no:721969465205588048> | You can't hack my owner!");
-    const toHack = args.slice(0).join(" ") && args.shift().toLowerCase();
-    message.delete();
+  if (!args[0])
+    return message.channel.send(
+      "<:redtick:719865119277842492> | Please mention the users!"
+    );
+  
+  const toHack = args.slice(0).join(" ") && args.shift().toLowerCase();
+  message.delete();
+  
+  if(toHack.id === "583649910092595232") return message.channel.send("You can't hack my owner!");
 
-    let msg = await message.channel.send(`Hacking ${toHack}...`);
-    console.log(`[HACK] Hacking to ${toHack}`);
+  let msg = await message.channel.send(`Hacking ${toHack}...`);
+  console.log(`[HACK] Hacking to ${toHack}`);
 
-    let time = "3s";
-    setTimeout(function() {
-      msg.edit("Hacking Email...");
-    }, ms(time));
+  let time = "3s";
+  setTimeout(function() {
+    msg.edit("Hacking Email...");
+  }, ms(time));
 
-    let time1 = "6s";
-    setTimeout(function() {
-      msg.edit(`Email: ${toHack}@gmail.com\n\`Pass: **********\nCode: 4S3D3_K0NT0L\``);
-      console.log(`[HACK] Email: ${toHack}@gmail.com`);
-    }, ms(time1));
+  let time1 = "6s";
+  setTimeout(function() {
+    msg.edit(
+      `Email: ${toHack}@gmail.com\n\`Pass: **********\nCode: 4S3D3_K0NT0L\``
+    );
+    console.log(`[HACK] Email: ${toHack}@gmail.com`);
+  }, ms(time1));
 
-    let time2 = "9s";
-    setTimeout(function() {
-      msg.edit(`Email of ${toHack} got hacked`);
-      console.log(`[HACK] Pass: ${toHack}123`);
-    }, ms(time2));
+  let time2 = "9s";
+  setTimeout(function() {
+    msg.edit(`Email of ${toHack} got hacked`);
+    console.log(`[HACK] Pass: ${toHack}123`);
+  }, ms(time2));
 
-    let time3 = "12s";
-    setTimeout(function() {
-      msg.edit(`Hacking ${toHack}'s Device...`);
-    }, ms(time3));
+  let time3 = "12s";
+  setTimeout(function() {
+    msg.edit(`Hacking ${toHack}'s Device...`);
+  }, ms(time3));
 
-    let time4 = "15s";
-    setTimeout(function() {
-      msg.edit(`Collect all ${toHack}'s Data...`);
-    }, ms(time4));
+  let time4 = "15s";
+  setTimeout(function() {
+    msg.edit(`Collect all ${toHack}'s Data...`);
+  }, ms(time4));
 
-    let time5 = "18s";
-    setTimeout(function() {
-      msg.edit(`Finding ip ${toHack}'s`);
-    }, ms(time5));
+  let time5 = "18s";
+  setTimeout(function() {
+    msg.edit(`Finding ip ${toHack}'s`);
+  }, ms(time5));
 
-    let time6 = "20s";
-    setTimeout(function() {
-      msg.edit(`Ip of ${toHack}= 13.66.220.110`);
-    }, ms(time6));
+  let time6 = "20s";
+  setTimeout(function() {
+    msg.edit(`Ip of ${toHack}= 13.66.220.110`);
+  }, ms(time6));
 
-    let time7 = "24s";
-    setTimeout(function() {
-      msg.edit(`DDoSing ${toHack}'s Ip!`);
-    }, ms(time7));
+  let time7 = "24s";
+  setTimeout(function() {
+    msg.edit(`DDoSing ${toHack}'s Ip!`);
+  }, ms(time7));
 
-    let time8 = "28s";
-    setTimeout(function() {
-      msg.edit(`${toHack} IP Success DDoSed`);
-    }, ms(time8));
+  let time8 = "28s";
+  setTimeout(function() {
+    msg.edit(`${toHack} IP Success DDoSed`);
+  }, ms(time8));
 
-    let time9 = "32s";
-    setTimeout(function() {
-      msg.edit(`Injecting Virus to ${toHack}'s All Device!`);
-    }, ms(time9));
+  let time9 = "32s";
+  setTimeout(function() {
+    msg.edit(`Injecting Virus to ${toHack}'s All Device!`);
+  }, ms(time9));
 
-    let time10 = "38s";
-    setTimeout(function() {
-      msg.edit(`Virus Injected to ${toHack}'s Device`);
-    }, ms(time10));
+  let time10 = "38s";
+  setTimeout(function() {
+    msg.edit(`Virus Injected to ${toHack}'s Device`);
+  }, ms(time10));
 
-    let time11 = "42s";
-    setTimeout(function() {
-      msg.edit(`<a:b_yes:721969088813072425> | ${toHack} Succesfully hacked.`);
-      console.log("[HACK] Hack was successful.");
-    }, ms(time11))
-      .then()
-      .catch(err => msg.reply("Please mention the user!"));
-  }
+  let time11 = "42s";
+  setTimeout(function() {
+    msg.edit(`<a:b_yes:721969088813072425> | ${toHack} Succesfully hacked.`);
+    console.log("[HACK] Hack was successful.");
+  }, ms(time11))
+    .then()
+    .catch(err => msg.reply("Please mention the user!"));
+};
 
 exports.help = {
-         name: "hack",
-         description: "hacking people",
-         usage: "hack <@mentions>",
-         example: "/hacl @McDunaldz",
+  name: "hack",
+  description: "hacking people",
+  usage: "hack <@mentions>",
+  example: "/hacl @McDunaldz"
 };
 
 exports.conf = {
-          aliases: [""],
-          cooldown: 5
+  aliases: [""],
+  cooldown: 5
 };
