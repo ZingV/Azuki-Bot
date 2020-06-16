799357798d5234da3cb858c91e1067e86016d964
Glitch (youthful-same-bed) <none>
2020-06-16 09:23:29 +0000

    😀🙎 Checkpoint ./commands/utility/covid.js:16213770/6090

--- commands/utility/covid.js
+++ commands/utility/covid.js
@@ -3,73 +3,78 @@ const fetch = require("node-fetch");
 const config = require("../../config.json");
 
 exports.run = async (client, message, args) => {
-    if (!args[0])
-      return message.channel.send("<:redtick:719865119277842492> | Please enter country you want to see stats!");
+  if (!args[0])
+    return message.channel.send(
+      "<:redtick:719865119277842492> | Please enter country you want to see stats!"
+    );
 
-    if (args.join(" ") === "all") {
-      let all = args[0];
-      fetch(`https://corona.lmao.ninja/v2/all`)
-        .then(res => res.text())
-        .then(json => {
-          var info = JSON.parse(json);
-          var embed = new Discord.MessageEmbed()
-            .setColor(config.color)
-            .setAuthor("Global")
-            .setThumbnail(
-              "https://cdn.discordapp.com/avatars/583649910092595232/93ab252b08f0a5de0bc3661bbb150afd.png?size=2048"
-            )
-            .addField("Population", info.population.toLocaleString())
-            .addField("Total Cases", info.cases.toLocaleString())
-            .addField("Total Active", info.active.toLocaleString())
-            .addField("Total Recovered", info.recovered.toLocaleString())
-            .addField("Today Infected", info.todayCases.toLocaleString())
-            .addField("Today Death", info.todayDeaths.toLocaleString())
-            .addField("Total Deaths", info.deaths.toLocaleString())
-            .addField("Total Critical", info.critical.toLocaleString())
-            .setFooter(`#STAYATHOME`)
-            .setTimestamp();
-          message.channel.send(embed);
-        });
-    } else {
-      let country = args[0];
-      fetch(`https://corona.lmao.ninja/v2/countries/${country}`)
-        .then(res => res.text())
-        .then(json => {
-          var info = JSON.parse(json);
-          var embed = new Discord.MessageEmbed()
-            .setColor(config.color)
-            .setAuthor(info.country)
-            .setThumbnail(info.countryInfo.flag)
-            .addField("Population", info.population.toLocaleString())
-            .addField("Total Cases", info.cases.toLocaleString())
-            .addField("Total Active", info.active.toLocaleString())
-            .addField("Total Recovered", info.recovered.toLocaleString())
-            .addField("Today Cases", info.todayCases.toLocaleString())
-            .addField("Today Deaths", info.todayDeaths.toLocaleString())
-            .addField("Total Deaths", info.deaths.toLocaleString())
-            .addField("Total Critical", info.critical.toLocaleString())
-            .setDescription("**Affected Country**\n215")
-            .setFooter(
-              `Want see global cases? type ${config.prefix}covid all | #STAYATHOME`
-            )
-            .setTimestamp();
+  if (args.join(" ") === "all") {
+    let all = args[0];
+    fetch(`https://corona.lmao.ninja/v2/all`)
+      .then(res => res.text())
+      .then(json => {
+        var info = JSON.parse(json);
+        var embed = new Discord.MessageEmbed()
+          .setColor(config.color)
+          .setAuthor("Global")
+          .setThumbnail(
+            "https://cdn.discordapp.com/avatars/583649910092595232/93ab252b08f0a5de0bc3661bbb150afd.png?size=2048"
+          )
+          .addField("Population", info.population.toLocaleString())
+          .addField("Total Cases", info.cases.toLocaleString())
+          .addField("Total Active", info.active.toLocaleString())
+          .addField("Total Recovered", info.recovered.toLocaleString())
+          .addField("Today Infected", info.todayCases.toLocaleString())
+          .addField("Today Death", info.todayDeaths.toLocaleString())
+          .addField("Total Deaths", info.deaths.toLocaleString())
+          .addField("Total Critical", info.critical.toLocaleString())
+          .setFooter(`#STAYATHOME`)
+          .setTimestamp();
+        message.channel.send(embed);
+      });
+  } else {
+    let country = args[0];
+    fetch(`https://corona.lmao.ninja/v2/countries/${country}`)
+      .then(res => res.text())
+      .then(json => {
+        var info = JSON.parse(json);
+        var embed = new Discord.MessageEmbed()
+          .setColor(config.color)
+          .setAuthor(info.country)
+          .setThumbnail(info.countryInfo.flag)
+          .addField("Population", info.population.toLocaleString())
+          .addField("Total Cases", info.cases.toLocaleString())
+          .addField("Total Active", info.active.toLocaleString())
+          .addField("Total Recovered", info.recovered.toLocaleString())
+          .addField("Today Cases", info.todayCases.toLocaleString())
+          .addField("Today Deaths", info.todayDeaths.toLocaleString())
+          .addField("Total Deaths", info.deaths.toLocaleString())
+          .addField("Total Critical", info.critical.toLocaleString())
+          .setDescription("**Affected Country**\n215")
+          .setFooter(
+            `Want see global cases? type ${config.prefix}covid all | #STAYATHOME`
+          )
+          .setTimestamp();
 
-          message.channel.send(embed)
-        
-         })
-        .then()
-        .catch(err => message.channel.send("<:redtick:719865119277842492> | Please enter the **Valid Country!**"));
-    }
-  };
+        message.channel.send(embed);
+      })
+      .then()
+      .catch(err =>
+        message.channel.send(
+          "<a:b_no:721969465205588048> | Please enter the **Valid Country!**"
+        )
+      );
+  }
+};
 
 exports.help = {
-         name: "covid",
-         description: "see covid stats in other country",
-         usage: "/covid all, /covid <country>",
-         example: "/covid all, /covid id",
+  name: "covid",
+  description: "see covid stats in other country",
+  usage: "/covid all, /covid <country>",
+  example: "/covid all, /covid id"
 };
 
 exports.conf = {
-          aliases: [""],
-          cooldown: 5
+  aliases: [""],
+  cooldown: 5
 };
