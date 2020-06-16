d98c1315366e753a3644f821206941c4fa58ed8b
Glitch (youthful-same-bed) <none>
2020-06-16 14:28:58 +0000

    👚🌑 Checkpoint ./commands/admin/addrole.js:16213770/3474 ./commands/admin/removerole.js:16213770/7021

--- commands/admin/addrole.js
+++ commands/admin/addrole.js
@@ -1,36 +1,57 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"]))
+    return message.channel.send(
+      "<:redtick:719865119277842492> | You dont have permission to perform this command!"
+    );
 
-    if(!message.member.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("<:redtick:719865119277842492> | You dont have permission to perform this command!")
+  let rMember = message.mentions.members.first();
+  if (!rMember)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | Please mention the users."
+    );
 
-    let rMember = message.mentions.members.first()
-    if(!rMember) return message.channel.send("<a:b_no:721969465205588048> | Please mention the users.")
-  
-  if(rMember === message.author.id) return message.channel.send("<a:b_no:721969465205588048> | You can't addrole to urself")
-  if(rMember === client.user.id) return message.channel.send("<a:b_no:721969465205588048> | You can't addrole to me")
-  
-    let role = message.guild.roles.find || message.mentions.roles.first()
-    if(!role) return message.channel.send("<a:b_no:721969465205588048> | Please mention the roles.") 
-    
-    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission.")
+  if (rMember === message.author.id)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | You can't addrole to urself"
+    );
+  if (rMember === client.user.id)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | You can't addrole to me"
+    );
 
-    if(rMember.roles.cache.has(role.id)) {
-        return message.channel.send(`<a:b_no:721969465205588048> | **${rMember.displayName}**, already has the role!`)
-    } else {
-        await rMember.roles.add(role.id).catch(e => console.log(e.message))
-        message.channel.send(`<a:b_yes:721969088813072425> | You has been added **${role}** to **${rMember.displayName}.**`)
-    }
+  let role = message.guild.roles.find || message.mentions.roles.first();
+  if (!role)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | Please mention the roles."
+    );
+
+  if (!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"]))
+    return message.channel.send("I don't have permission.");
+
+  if (rMember.roles.cache.has(role.id)) {
+    return message.channel.send(
+      `<a:b_no:721969465205588048> | **${rMember.displayName}**, already has the role!`
+    );
+  } else {
+    await rMember.roles
+      .add(role.id)
+      .catch(e => message.channel.send(`<a:b_no:721969465205588048> | **${e.message}**`));
+    return message.channel.send(
+      `<a:b_yes:721969088813072425> | You has been added **${role}** to **${rMember.displayName}.**`
+    );
+  }
 };
 
 exports.help = {
-         name: "addrole",
-         description: "addrole to members",
-         usage: "addrole <@mentions> <@role>",
-         example: "/addrole @McDunaldz @Member",
+  name: "addrole",
+  description: "addrole to members",
+  usage: "addrole <@mentions> <@role>",
+  example: "/addrole @McDunaldz @Member"
 };
 
 exports.conf = {
-          aliases: ["promote"],
-          cooldown: 5
+  aliases: ["promote"],
+  cooldown: 5
 };
--- commands/admin/removerole.js
+++ commands/admin/removerole.js
@@ -1,36 +1,57 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"]))
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | You dont have permission to perform this command!"
+    );
 
-    if(!message.member.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("<a:b_no:721969465205588048> | You dont have permission to perform this command!")
+  let rMember = message.mentions.members.first();
+  if (!rMember)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | Please mention the users."
+    );
 
-    let rMember = message.mentions.members.first()
-    if(!rMember) return message.channel.send("<a:b_no:721969465205588048> | Please mention the users.")
-  
-  if(rMember === message.author.id) return message.channel.send("<a:b_no:721969465205588048> | You can't removerole from urself")
-  if(rMember === client.user.id) return message.channel.send("<a:b_no:721969465205588048> | You can't remove my role")
-  
-    let role = message.guild.roles.find || message.mentions.roles.first()
-    if(!role) return message.channel.send("<a:b_no:721969465205588048> | Please mention the roles.") 
+  if (rMember === message.author.id)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | You can't removerole from urself"
+    );
+  if (rMember === client.user.id)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | You can't remove my role"
+    );
 
-    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission.")
+  let role = message.guild.roles.find || message.mentions.roles.first();
+  if (!role)
+    return message.channel.send(
+      "<a:b_no:721969465205588048> | Please mention the roles."
+    );
 
-    if(!rMember.roles.cache.has(role.id)) {
-        return message.channel.send(`<a:b_no:721969465205588048> | **${rMember.displayName}**, doesnt have the role!`)
-    } else {
-        await rMember.roles.remove(role.id).catch(e => console.log(e.message))
-        message.channel.send(`<a:b_yes:721969088813072425> | You has been removed ${role} from **${rMember.displayName}**.`)
-    }
-}
+  if (!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"]))
+    return message.channel.send("I don't have permission.");
+
+  if (!rMember.roles.cache.has(role.id)) {
+    return message.channel.send(
+      `<a:b_no:721969465205588048> | **${rMember.displayName}**, doesnt have the role!`
+    );
+  } else {
+    await rMember.roles
+      .remove(role.id)
+      .catch(e => message.channel.send(`<a:b_no:721969465205588048> | **${e.message}**`));
+    return message.channel.send(
+      `<a:b_yes:721969088813072425> | You has been removed ${role} from **${rMember.displayName}**.`
+    );
+  }
+};
 
 exports.help = {
-         name: "removerole",
-         description: "remove roles from members",
-         usage: "demote <@mentions> <@roles>",
-         example: "/demote @McDunaldz @Youtuber",
+  name: "removerole",
+  description: "remove roles from members",
+  usage: "demote <@mentions> <@roles>",
+  example: "/demote @McDunaldz @Youtuber"
 };
 
 exports.conf = {
-          aliases: ["demote"],
-          cooldown: 5
+  aliases: ["demote"],
+  cooldown: 5
 };
