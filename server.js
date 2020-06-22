6d01132c1ad6fe505283adda47fa3b3a6b265e61
Glitch (youthful-same-bed) <none>
2020-06-22 11:40:39 +0000

    🏦🏙 Checkpoint ./server.js:16213770/523

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log('Pinging');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const Discord = require("discord.js");
 const tutorialBot = require("./handler/ClientBuilder.js"); // We're gonna create this soon.
 const client = new tutorialBot();
