e04d5dfde06ed897eddf35a0b7dd10e07191c841
Glitch (youthful-same-bed) <none>
2020-06-27 03:55:26 +0000

    🐀🗻 Checkpoint ./commands/owner/serverlist.js:16213770/1681

--- commands/owner/serverlist.js
+++ commands/owner/serverlist.js
@@ -1,24 +1,28 @@
-const Discord = require("discord.js")
-const config = require("../../config.json")
+const Discord = require("discord.js");
+const config = require("../../config.json");
 
 exports.run = async (client, message, args) => {
-  const guildName = client.guilds.cache.map(a => a.name)
-  const guildId = client.guilds.cache.map(a => a.id)
+  if (message.author.id !== "583649910092595232") return;
+  const guildName = client.guilds.cache.map(a => a.name).join("\n");
   
   const embed = new Discord.MessageEmbed()
-  .setColor(config.color)
-  .addField(guildName)
-  
-  message.channel.send(embed)
-}
+    .setColor(config.color)
+    .setAuthor(
+      client.user.username + " Server List!",
+      client.user.displayAvatarURL()
+    )
+    .setDescription(`**${guildName}**`);
+
+  message.channel.send(embed);
+};
 
 exports.help = {
   name: "serverlist",
   description: "Check server list",
   usage: "serverlist"
-}
+};
 
 exports.conf = {
   aliases: [""],
   cooldown: 0
-}
+};
