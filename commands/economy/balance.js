4112471a02774f582358e13fdbbed1383417d95d
Glitch (ordinarys) <none>
2020-06-28 14:53:44 +0000

    🏚🕹 Checkpoint ./commands/economy/balance.js:16213770/1663

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,14 +1,19 @@
-const Discord = require("discord.js")
-const db = require("quick.db")
+const Discord = require("discord.js");
+const db = require("quick.db");
+const config = require("../../config.json");
 
 exports.run = async (client, message, args) => {
-  let user = message.mentions.users.first() || message.author
-        let money = db.fetch(`money_${user.id}`)
+  let user = message.mentions.users.first() || message.author;
+  let money = db.fetch(`money_${user.id}`);
 
-        if (money === null) money = 0
+  if (money === null) money = 0;
 
-        message.channel.send(`**${user}** have **${money}** coin`)
-}
+  var embed = new Discord.MessageEmbed()
+    .setColor(config.color)
+    .setDescription(`**${user}** have **${money}** coin`);
+
+  message.channel.send(embed);
+};
 
 exports.help = {
   name: "balance",
