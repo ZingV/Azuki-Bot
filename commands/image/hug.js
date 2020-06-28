a0fa47dfb572653ea1ecb4788426639382fce29f
Glitch (ordinarys) <none>
2020-06-28 15:16:49 +0000

    👖🚶 Checkpoint ./commands/image/hug.js:16213770/2970

--- commands/image/hug.js
+++ commands/image/hug.js
@@ -2,6 +2,20 @@ const Discord = require("discord.js");
 const config = require("../../config.json");
 
 exports.run = async (client, message, args) => {
+  function getUserFromMention(mention) {
+    if (mention.startsWith("<@") && mention.endsWith(">")) {
+      mention = mention.slice(2, -1);
+
+      if (mention.startsWith("!")) {
+        mention = mention.slice(1);
+      }
+
+      return client.user.cache.get(mention);
+    }
+  }
+
+  const user = message.mentions.users.first() || client.user;
+
   let gif = [
     "https://media.tenor.com/images/1e058dc8d0ccd337b6d26cbab43b6e30/tenor.gif",
     "https://media.tenor.com/images/bc8e962e6888249486a3e103edd30dd9/tenor.gif",
@@ -11,7 +25,8 @@ exports.run = async (client, message, args) => {
   var embed = new Discord.MessageEmbed()
     .setColor(config.color)
     .setImage(gif[Math.floor(Math.random() * gif.length)])
-    .setAuthor("Hug Gif");
+    .setAuthor(`${message.author.username} Hug ${user.username}`)
+    .setTimestamp(new Date());
 
   message.channel.send(embed);
 };
