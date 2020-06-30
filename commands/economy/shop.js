3668a094cf6d716f14225f3765ab23508f8aad21
Glitch (ordinarys) <none>
2020-06-30 13:44:47 +0000

    🏡💏 Checkpoint ./commands/economy/shop.js:16213770/36

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -7,18 +7,18 @@ exports.run = async (client, message, args) => {
     .setColor("#00bfff")
     .addField(
       "<:rods:721718010070434136> | Fishing Rod",
-      "id: rod\ncost: 1,000 coins"
+      "```id: rod\ncost: 1,000 coins```"
     )
     .addField(
       "<:swords:721721896059797525> | Sword",
-      "id: sword\ncost: 700 coins"
+      "```id: sword\ncost: 700 coins```"
     )
     .addField(
       "<:pick:721715584752156753> | Pickaxe",
-      "id: pick\ncost: 1,200 coins"
+      "```id: pick\ncost: 1,200 coins```"
     )
-    .addField("<:guns:721722603219189801> | Gun", "id: gun\ncost: 10,000 coins")
-    .addField("<:tea:721723802211450910> | Tea", "id: tea\ncost: 5 coins");
+    .addField("<:guns:721722603219189801> | Gun", "```id: gun\ncost: 10,000 coins```")
+    .addField("<:tea:721723802211450910> | Tea", "```id: tea\ncost: 5 coins```");
   message.channel.send(embed);
 };
 
