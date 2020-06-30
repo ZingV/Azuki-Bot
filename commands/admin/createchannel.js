e40fa50b0d9f0f3d463da01dfe233a512e36a014
Glitch (ordinarys) <none>
2020-06-30 17:13:30 +0000

    🐉👨‍👨‍👦 Checkpoint ./commands/admin/createchannel.js:16213770/3359

--- commands/admin/createchannel.js
+++ commands/admin/createchannel.js
@@ -1,16 +1,29 @@
-exports.run = async (client, message,args) => {
-  if(!message.members.hasPermissions("MANAGE_CHANNELS")) return message.chsnnel.send("You don't have permission!")
-  if(args[1]) return message.channel.send("Please Input Channel Type
-  
+exports.run = async (client, message, args) => {
+try {
+  if (!message.member.hasPermission("MANAGE_CHANNELS"))
+    return message.channel.send("You don't have permission!");
+  if (args[1]) return message.channel.send("Please Input Channel Type!");
+  if (args[0])
+    return message.channel.send("Please Include The Name For Channels!");
+
+  message.channel.send("Channel has been created").then(() => {
+    message.guild.channels
+      .create(args[1], { type: args[0] }, [])
+      .then()
+      .catch(err => message.channel.send("Something error!"));
+  });
+} catch(e) {
+  message.channel.send(e.message)
 }
+};
 
 exports.help = {
   name: "createchannel",
   description: "Creating Channel, Using Command!",
   usage: "createchannel <text/voice> <name>"
-}
+};
 
 exports.conf = {
   aliases: [""],
   cooldown: 2
-}
+};
