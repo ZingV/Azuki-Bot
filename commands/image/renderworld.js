eaeefac412d3618c29914e6def2f308df9dee25c
Glitch (ordinarys) <none>
2020-07-07 15:33:34 +0000

    ⏳🍉 Checkpoint ./commands/image/renderworld.js:16213770/392

--- commands/image/renderworld.js
+++ commands/image/renderworld.js
@@ -1,7 +1,13 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message, args) => {
+  if(!args[0]) return message.channel.send("Please input name world!")
+  let world = args[0]
+  let embed = new discord.MessageEmbed()
+  .setAuthor(`> Name World: ${world}`)
+  .setImage(`https://s3.amazonaws.com/world.growtopiagame.com/${world}.png`)
   
+  message.channel.send(embed)
 }
 
 exports.help = {
