579497c39f602476bd69117b8b1018e698ff2f58
Glitch (ordinarys) <none>
2020-07-12 09:41:01 +0000

    🏩🎓 Checkpoint ./handler/module.js:16213770/22 ./commands/information/help.js:16213770/73 ./watch.json:16213770/11

--- handler/module.js
+++ handler/module.js
@@ -32,7 +32,7 @@ module.exports = client => {
           
           client.commands.set(prop.help.name, prop)
           
-          prop.conf.aliases(alias => {
+          prop.conf.aliases.forEach(alias => {
             client.aliases.set(alias, prop.help.name);
           })
           
