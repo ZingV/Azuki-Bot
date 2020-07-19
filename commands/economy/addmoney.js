f49ae9e5a5188d0f5e70c3ca36a1b7fde9b7fafd
Glitch (ordinarys) <none>
2020-07-19 15:05:07 +0000

    🎨🌏 Checkpoint ./commands/economy/addmoney.js:16213770/4122 ./watch.json:16213770/5

--- commands/economy/addmoney.js
+++ commands/economy/addmoney.js
@@ -1,25 +1,26 @@
 const Discord = require("discord.js");
-const db = require("quick.db")
+const db = require("quick.db");
 
 exports.run = async (client, message, args) => {
-  if(!client.config.owners.includes(message.author.id));
-  return message.channel.send("<a:b_no:721969465205588048> | Only owner bot can using this command!")
-    
-  let amount = args[0]
+  let amount = args[0];
+  if (!amount) return message.channel.send("Please specify amount");
+
   let user = message.mentions.users.first() || message.author;
-  
+
   db.add(`money_${message.author.id}`, amount);
-  
-  message.channel.send(`Successfully Added 💴 **${amount}** To ${user}`)
-}
+
+  message.channel.send(
+    `<a:b_yes:721969088813072425> | Successfully Added 💴 **${amount}** To ${user}`
+  );
+};
 
 exports.help = {
   name: "addmoney",
   description: "Adding money to ur balance",
   usage: "addmoney <amount>"
-}
+};
 
 exports.conf = {
   aliases: ["cheat"],
   cooldown: 0
-}
+};
