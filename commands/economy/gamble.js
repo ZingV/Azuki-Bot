1ba657504e02bf1e2c2e61923bac629b6d47d882
Glitch (ordinarys) <none>
2020-07-19 14:06:34 +0000

    🤗🚕 Checkpoint ./commands/economy/gamble.js:16213770/250

---
+++ commands/economy/gamble.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+const ms = require("parse-ms")
+
+exports.run = async (client, message, args) => {
+  
+}
+
+exports.help = {
+  
+}
