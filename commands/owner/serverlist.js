2c811127491bb76e8a1795993b7e0acbd219d330
Glitch (ordinarys) <none>
2020-07-21 14:41:10 +0000

    🙉🛳 Checkpoint ./commands/owner/serverlist.js:16213770/1996 ./watch.json:16213770/1

--- commands/owner/serverlist.js
+++ commands/owner/serverlist.js
@@ -4,14 +4,20 @@ const config = require("../../config.json");
 exports.run = async (client, message, args) => {
   if (message.author.id !== "583649910092595232") return;
   const guildName = client.guilds.cache.map(a => a.name).join("\n");
-  
+
   const embed = new Discord.MessageEmbed()
     .setColor(config.color)
+    .setImage(
+      "https://www.gambaranimasi.org/data/media/562/animasi-bergerak-garis-0031.gif"
+    )
     .setAuthor(
       client.user.username + ` Server List [${client.guilds.cache.size}]:`,
       client.user.displayAvatarURL()
     )
-    .setDescription(`**${guildName}**`);
+    .setDescription(`**${guildName}**`)
+    .setImage(
+      "https://www.gambaranimasi.org/data/media/562/animasi-bergerak-garis-0031.gif"
+    );
 
   message.channel.send(embed);
 };
