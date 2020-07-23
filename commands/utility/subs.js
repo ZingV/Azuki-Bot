6002a4956353ac9434c26be14e4a18de5a0081e3
Glitch (bot-harams) <none>
2020-07-23 10:32:21 +0000

    ✨🎠 Checkpoint ./commands/utility/subs.js:16213770/370

--- commands/utility/subs.js
+++ commands/utility/subs.js
@@ -3,9 +3,21 @@ const cheerio = require("cheerio")
 const request = require("request-promise")
 
 exports.run = async (client, message, args) => {
-  
+  if(!args[0])
+    return message.channel.send("Please enter yt url")
+  let yturl = args[0]
+  let response = await request(yturl)
+  let cok = cheerio.load(response)
+  let subscount = cok('').attr('title');
 }
 
 exports.help = {
-  
+  name: "subs",
+  description: "Viewing Ur Subs Count",
+  usage: "subs"
+  }
+
+exports.conf = {
+  aliases: [],
+  cooldown: 2
   }
