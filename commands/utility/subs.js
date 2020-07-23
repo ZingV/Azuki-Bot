1a29e094e6bd22c66a8b21e106a3dfdc2933a557
Glitch (bot-harams) <none>
2020-07-23 10:20:18 +0000

    🚌📡 Checkpoint ./package.json:16213770/1793 ./commands/utility/subs.js:16213770/252

---
+++ commands/utility/subs.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const cheerio = require("cheerio")
+const request = require("request-promise")
+
+exports.run = async (client, message, args) => {
+  
+}
+
+exports.help = {
+  
+  }
