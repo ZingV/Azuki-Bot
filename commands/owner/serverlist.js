cb4f92ff9d8913df9915f718689e5aa08f205977
Glitch (bot-harams) <none>
2020-07-31 10:21:37 +0000

    🍂🎅 Checkpoint ./commands/owner/serverlist.js:16213770/4339

--- commands/owner/serverlist.js
+++ commands/owner/serverlist.js
@@ -1,33 +1,25 @@
-const Discord = require("discord.js");
-const config = require("../../config.json");
+const Discord = require ("discord.js");
 
 exports.run = async (client, message, args) => {
-  if (message.author.id !== "583649910092595232") return;
+  if(message.author.id !== "583649910092595232") return
   
-  let guild = client.guilds.cache.map(a => a.name).join("\n")
+  const name = client.guilds.cache.map(a => a.name)
   
-  const embed = new Discord.MessageEmbed()
-    .setColor(config.color)
-    .setAuthor(
-      client.user.username + ` Server List [${client.guilds.cache.size}]:`,
-      client.user.displayAvatarURL()
-    )
-    .setDescription(`**${guild}**`)
-    .setImage(
-      "https://www.gambaranimasi.org/data/media/562/animasi-bergerak-garis-0031.gif"
-    )
-    .setTimestamp(new Date());
-
-  message.channel.send(embed);
-};
+  let embed = new Discord.MessageEmbed()
+  .setColor("#00bfff")
+  .setAuthor(`${client.user.username} Server List`)
+  .setDescription(`**${name}**`)
+  .setTimestamp(new Date())
+  .setImage(
+}
 
 exports.help = {
   name: "serverlist",
-  description: "Check server list",
+  description: "Server list",
   usage: "serverlist"
-};
+}
 
 exports.conf = {
-  aliases: [""],
+  aliases: [],
   cooldown: 0
-};
+}
