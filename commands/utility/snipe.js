dd00e5dc876259cbd209c4613f49954ae66af582
Glitch (clayne) <none>
2020-09-01 02:37:21 +0000

    💸🍿 Checkpoint ./server.js:16213770/371 ./commands/utility/snipe.js:16213770/78

--- commands/utility/snipe.js
+++ commands/utility/snipe.js
@@ -7,9 +7,10 @@ exports.run = async (client, message, args) => {
   const msg = client.snipes.get(message.channel.id)
     if(!msg) return message.channel.send("I don't see any stored deleted message here.")
     const embed = new Discord.MessageEmbed()
-    .setAuthor(msg.author, msg.author.displayAvatarURL())
-    .setColor("ffed00")
+    .setAuthor(msg.author, msg.avatar)
+    .setColor("#00bfff")
     .setDescription(msg.content)
+    .setTimestamp(new Date())
     if(msg.image)embed.setImage(msg.image)
     
     message.channel.send(embed)
