647c793672febad2d4d4a35f06bcccf107b74bf0
Glitch (clayne) <none>
2020-09-08 09:49:34 +0000

    🏚💎 Checkpoint ./commands/admin/badword.js:16213770/588

--- commands/admin/badword.js
+++ commands/admin/badword.js
@@ -3,37 +3,42 @@ const Discord = require("discord.js");
 
 exports.run = async(client, message, args) => {
   if(args[0] === "add") {
+    let a = db.fetch(`bl_${message.guild.id}`)
+    
     let word = args.slice(3)
     
     if(!word)
-      return message.channel.send("Please input the word!")
+      return message.channel.send("Please input the word!");
+    
+    db.push(`bl_${message.guild.id}`, word);
+    
+    message.channel.send(`\`${word}\` has been added to the list`)
+  }
+  
+  if(args[0] === "delete") {
+    db.delete(`bl_${message.guild.id}`)
+    
+    message.channel.send("Successfully delete all words!")
   }
   
   if(args[0] === "list") {
+    let all = db.fetch(`bl_${message.guild.id}`)
     
+    if(all ===n
+    
+    message.channel.send(all.join(", "))
   }
   
   if(args[0] === "remove") {
     
   }
   
-  else {
-  let embed = new Discord.MessageEmbed()
-  .setColor("#00bfff")
-  .setAuthor("Badword System", message.guild.iconURL())
-  .addField("add", `\`\`\`Add word to blacklist\`\`\``)
-  .addField("list", `\`\`\`Show all list badword\`\`\``)
-  .addField("remove", `\`\`\`Remove word from blacklist\`\`\``)
-  .setFooter(`This Command Under Development By ${client.config.owners.username}`)
-  
-  message.channel.send(embed)
-  }
 }
 
 exports.help = {
   name: "badword",
   description: "Badword system",
-  usage: "badword add <word>, badword list, badword remove"
+  usage: "badword add <word>, badword list, badword delete, badword remove <word>"
 }
 
 exports.conf = {
