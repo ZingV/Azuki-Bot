3be7f1d57d0d0865975b78e7c806f4272afbf6a3
Glitch (clayne) <none>
2020-09-08 13:51:33 +0000

    👆💋 Checkpoint ./events/ready.js:16213770/107 ./events/message.js:16213770/8403 ./watch.json:16213770/1

--- events/ready.js
+++ events/ready.js
@@ -8,9 +8,6 @@ module.exports = client => {
   
 
   client.on("message", async message => {
-  if(message.content === "dhani" || message.content === "Dhani")
-    message.reply("He Was Retarded")
-    
     if(message.author.bot) return;
     if(!message.guild.id) return
     
