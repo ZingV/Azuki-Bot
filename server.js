432083842a09608aefb39b5ac4915e4b41492e49
Glitch (clayne) <none>
2020-09-09 16:48:02 +0000

    🎐⛱ Checkpoint ./server.js:16213770/6 ./watch.json:16213770/1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static("public"))
 
 app.get("/", (request, response) => {
   console.log("Ping received");
-  response.sendStatus(301)
+  response.sendStatus(200)
 })
 app.listen(process.env.PORT);
 setInterval(() => {
