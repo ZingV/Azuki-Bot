83bcfebb6ad69d2a178c8d1fcc7b3ff3b122b129
Glitch (clayne) <none>
2020-10-02 21:34:41 +0000

    🌆🍽 Checkpoint ./commands/owner/serverlist.js:23156222/899 ./watch.json:23156222/2

--- commands/owner/serverlist.js
+++ commands/owner/serverlist.js
@@ -3,12 +3,36 @@ const Discord = require ("discord.js");
 exports.run = async (client, message, args) => {
   if(message.author.id !== "583649910092595232") return
   
-  const name = client.guilds.cache.map(a => a.name)
+  let obj = [] 
+                
+        client.guilds.cache.map(async x => {
+          //await x.fetchMembers() 
+          obj.push({
+            name: x.name,
+            members: x.memberCount,
+            ownerID: x.ownerID,
+            id: x.id
+          }) 
+        }) 
+       
+        //await new Promise(resolve => setTimeout(resolve, 2500))
+        
+        let content = []
+        
+        let size = obj.length
+        
+        if (size > 10) size = 10
+        
+        obj = obj.sort((x, y) => y.members - x.members) 
+        
+        for (let i = 0;i < size;i++) {  
+          content.push(`**${i + 1}# - ${obj[i].name}**:\nGuild ID: **${obj[i].id}**\nOwner ID: **${obj[i].ownerID}**\nMember Count: **${obj[i].members}** Users`) 
+        }
   
   let embed = new Discord.MessageEmbed()
   .setColor("#00bfff")
   .setAuthor(`${client.user.username} Server List [${client.guilds.cache.size}]:`)
-  .setDescription(`**${name}**`)
+  .setDescription(content.join("\n\n"))
   .setTimestamp(new Date())
   .setImage("https://www.gambaranimasi.org/data/media/562/animasi-bergerak-garis-0031.gif")
   
