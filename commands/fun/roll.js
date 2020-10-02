ac2b17a88927b878526bdf8473fd32ce9fdcc5f0
Glitch (clayne) <none>
2020-10-02 11:12:52 +0000

    🎻🙉 Checkpoint ./watch.json:23156222/2 ./commands/fun/roll.js:23156222/15

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,13 +1,13 @@
 const { MessageEmbed } = require("discord.js")
 
 exports.run = async(client, message, args) => {
-  let result = Math.floor(Math.random() * 6);
+  let result = Math.floor(Math.random() * 32);
   
   if (result == 0) {
     let embed = new MessageEmbed()
     .setColor("00ff00")
     .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
-    .setDescription(`**${message.author.username}** spun the wheel, You got **0**!`)
+    .setDescription(`**${message.author.username}** spun the wheel, And got **0**!`)
     .setTimestamp()
     message.channel.send(embed);
   }
@@ -15,7 +15,7 @@ exports.run = async(client, message, args) => {
     let embeds = new MessageEmbed()
     .setColor("RANDOM")
     .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
-    .setDescription(`**${message.author.username}** spun the wheel, You got **${result}**!`)
+    .setDescription(`**${message.author.username}** spun the wheel, And got **${result}**!`)
     .setTimestamp()
     message.channel.send(embeds);
   }
