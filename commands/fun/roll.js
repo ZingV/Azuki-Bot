2049d3e054cab7636731ef55024a4335612a8321
Glitch (clayne) <none>
2020-10-02 10:49:16 +0000

    🌼👆 Checkpoint ./commands/fun/roll.js:23156222/677 ./watch.json:23156222/7

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -7,17 +7,18 @@ exports.run = async(client, message, args) => {
     let embed = new MessageEmbed()
     .setColor("00ff00")
     .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
-    .setDescription(`<:roulette:761534110937120779> ${message.author.username} spun the wheel, You got **0**!`)
+    .setDescription(`**${message.author.username}** spun the wheel, You got **0**!`)
     .setTimestamp()
     message.channel.send(embed);
   }
-  
-  let embeds = new MessageEmbed()
-  .setColor("#FF0000")
-  .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
-  .setDescription(`<:roulette:761534110937120779> ${message.author.username} spun the wheel, You got **${result}**!`)
-   .setTimestamp()
-  message.channel.send(embeds);
+  else {
+    let embeds = new MessageEmbed()
+    .setColor("#FF0000")
+    .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
+    .setDescription(`**${message.author.username}** spun the wheel, You got **${result}**!`)
+    .setTimestamp()
+    message.channel.send(embeds);
+  }
 }
 
 exports.help = {
