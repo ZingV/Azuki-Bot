e6eab77ba409db2e394321eb6f627cc520b5271a
Glitch (clayne) <none>
2020-10-02 10:37:47 +0000

    💃💄 Checkpoint ./commands/fun/roll.js:23156222/1370 ./watch.json:23156222/1

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -6,6 +6,27 @@ exports.run = async(client, message, args) => {
   if (result == 0) {
     let embed = new MessageEmbed()
     .setColor("00ff00")
-    .setDescription(``)
+    .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
+    .setDescription(`<:roulette:761534110937120779> ${message.author.username} spun the wheel, You got **0**!`)
+    .setTimestamp()
+    message.channel.send(embed);
   }
+  
+  let embeds = new MessageEmbed()
+  .setColor("#FF0000")
+  .setAuthor("Roulette Wheel", "https://vignette.wikia.nocookie.net/growtopia/images/8/8f/ItemSprites.png/revision/latest/window-crop/width/32/x-offset/544/y-offset/64/window-width/32/window-height/32?fill=cb-20201001200938")
+  .setDescription(`<:roulette:761534110937120779> ${message.author.username} spun the wheel, You got **${result}**!`)
+   .setTimestamp()
+  message.channel.send(embeds);
+}
+
+exports.help = {
+  name: "roll",
+  description: "Roulette wheel for fun",
+  usage: "roll"
+}
+
+exports.conf = {
+  aliases: [],
+  cooldown: 5
 }
