81b47f6ca942987f39ae3bfa55055d479d399177
Glitch (clayne) <none>
2020-10-03 06:08:20 +0000

    🌓⏰ Checkpoint ./commands/fun/reverse.js:23156222/679

---
+++ commands/fun/reverse.js
@@ -0,0 +1,18 @@
+exports.run = async (client, message, args) => {
+  let a = args.slice(0).join(" ");
+  
+  if (!a) return message.channel.send("Enter your text!");
+  
+  let reverse = a.split("").reverse().join("")
+  
+  let loadmsg = await message.channel.send("a:loading:746163270414762184> Reversing ur text....").then(a => a.delete({ timeout: 1000}));
+  
+  message.channel.send("Here ur text: " + reverse);
+}
+
+exports.help = {
+  name: "reverse",
+  description: "Reversing text",
+  usage: "reverse <text>"
+};
+
