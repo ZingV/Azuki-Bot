30ed693fc39ab972c72ea41b4e9a23f9e914ec96
Glitch (clayne) <none>
2020-10-03 01:05:07 +0000

    📊🌽 Checkpoint ./commands/utility/translate.js:23156222/361

--- commands/utility/translate.js
+++ commands/utility/translate.js
@@ -1,11 +1,15 @@
 const translate = require('@k3rn31p4nic/google-translate-api');
 
 exports.run = async(client, message, args) => {
+  
+  if (!args[0]) return message.channel.send("Insert Language!")
+  
+  if(!args.slice(1).join(" ")) return message.channel.send("Insert what text to translate!")
 
     translate(args.slice(1).join(" "), { to: args[0] }).then(res => {
-      message.channel.send(res.text);
+      message.channel.send(`${res.text}`);
   }).catch(err => {
-      console.error(err);
+      message.channel.send(err);
   });
 }
 
